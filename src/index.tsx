abbe3b84ac1b6ee0a50630738e7c893a4c80f198
Alex Hernandez <alexh385@gmail.com>
2019-02-19 01:48:05 -0500

    Refactor index.tsx to incorporate redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,21 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { EnthusiasmAction } from './actions/index';
+import Hello from './containers/Hello';
 import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+import { enthusiasm } from './reducers/index';
+import { IStoreState } from './types/index';
+
+const store = createStore<IStoreState, EnthusiasmAction, any, any>(enthusiasm, {
+  enthusiasmLevel: 1,
+  languageName: 'TypeScript',
+});
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <Hello />
+  </Provider>,
   document.getElementById('root') as HTMLElement
 );
-registerServiceWorker();
