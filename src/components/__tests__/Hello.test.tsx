0b42d5aac5e3782ed42762a3a0af4571e6ad034c
Alex Hernandez <alexh385@gmail.com>
2019-02-20 00:34:23 -0500

    Refactor directory structure for tests

--- src/components/__tests__/Hello.test.tsx
+++ src/components/__tests__/Hello.test.tsx
@@ -1,6 +1,6 @@
 import * as enzyme from 'enzyme';
 import * as React from 'react';
-import Hello from './Hello';
+import Hello from '../Hello';
 
 it('renders the correct text when no enthusiasm level is given', () => {
     const hello = enzyme.shallow(<Hello name='Alex' />);
