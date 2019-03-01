87fe4e0a5db0b0bd7ad307f7c1d660701357f3f9
Alex Hernandez <alexh385@gmail.com>
2019-03-01 02:41:18 -0500

    Add Root file for Full DOM rendering testing

---
+++ src/Root.tsx
@@ -0,0 +1,18 @@
+import { EnthusiasmAction } from 'actions';
+import * as React from 'react';
+import { Provider } from 'react-redux';
+import enthusiasm from 'reducers/enthusiasm';
+import { createStore } from 'redux';
+// import { createMockStore } from 'redux-test-utils';
+import { IStoreState } from 'types';
+ 
+export default (props: any) => {
+
+    const store = createStore<IStoreState, EnthusiasmAction, any, any>(enthusiasm, props.initialState);
+
+    return (
+        <Provider store={store} >
+            {props.children}
+        </Provider>
+    );
+}
